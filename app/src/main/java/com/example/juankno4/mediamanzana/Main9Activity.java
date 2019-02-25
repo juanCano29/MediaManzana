475581b2cca7f0458f128ad136f2f470b82bd705
juanCano29 <juankno4@gmail.com>
2019-02-25 17:19:29 -0600

    toolbar

--- app/src/main/java/com/example/juankno4/mediamanzana/Main9Activity.java
+++ app/src/main/java/com/example/juankno4/mediamanzana/Main9Activity.java
@@ -2,7 +2,7 @@ package com.example.juankno4.mediamanzana;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.widget.Toolbar;
 
 public class Main9Activity extends AppCompatActivity {
 
@@ -10,13 +10,8 @@ public class Main9Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main9);
+        Toolbar tool = findViewById(R.id.toolbar2);
+        setSupportActionBar(tool);
 
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-         getMenuInflater().inflate(R.menu.menu, menu);
-         return true;
     }
 }
