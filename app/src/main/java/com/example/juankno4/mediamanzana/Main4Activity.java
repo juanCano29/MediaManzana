e66c13b441d8310f8fc3067c037fae7ca9c2c725
juanCano29 <juankno4@gmail.com>
2019-02-25 20:58:16 -0600

    Navegacion entre actividades

--- app/src/main/java/com/example/juankno4/mediamanzana/Main4Activity.java
+++ app/src/main/java/com/example/juankno4/mediamanzana/Main4Activity.java
@@ -1,13 +1,35 @@
 package com.example.juankno4.mediamanzana;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main4Activity extends AppCompatActivity {
+Button btnno, btnsigo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+      btnno = findViewById(R.id.btnno);
+      btnsigo = findViewById(R.id.btnsigo);
+
+      btnno.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              onBackPressed();
+              finish();
+          }
+      });
+      btnsigo.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              Intent ir = new Intent(Main4Activity.this, Main9Activity.class);
+              startActivity(ir);
+          }
+      });
+
     }
 }
--- app/src/main/java/com/example/juankno4/mediamanzana/Main5Activity.java
+++ app/src/main/java/com/example/juankno4/mediamanzana/Main5Activity.java
@@ -1,13 +1,25 @@
 package com.example.juankno4.mediamanzana;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main5Activity extends AppCompatActivity {
-
+Button btnlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+        btnlogin = findViewById(R.id.btnlogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent li = new Intent(Main5Activity.this,Main9Activity.class);
+                startActivity(li);
+            }
+        });
     }
 }
