7f18d8ebad84c8fe572a73ae2b4ae554c95cad0d
juanCano29 <juankno4@gmail.com>
2019-02-25 18:07:35 -0600

    toolbar

--- app/src/main/java/com/example/juankno4/mediamanzana/Main9Activity.java
+++ app/src/main/java/com/example/juankno4/mediamanzana/Main9Activity.java
@@ -3,6 +3,10 @@ package com.example.juankno4.mediamanzana;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 
 public class Main9Activity extends AppCompatActivity {
 
@@ -12,6 +16,32 @@ public class Main9Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main9);
         Toolbar tool = findViewById(R.id.toolbar2);
         setSupportActionBar(tool);
+        getSupportActionBar().setTitle(null);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.conf:
+
+                return true;
+            case R.id.acercade:
+                 
+                return true;
+            case R.id.contact:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
