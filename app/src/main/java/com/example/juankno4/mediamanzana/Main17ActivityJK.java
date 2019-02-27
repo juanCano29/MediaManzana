074eb54a35e7465144f58567163392cf25a3b703
kiikedls <jespinozadelossatnos@gmail.com>
2019-02-26 22:29:24 -0600

    cree el activity de contactos y redireccione todos los apartados de los tres puntitos

---
+++ app/src/main/java/com/example/juankno4/mediamanzana/Main17ActivityJK.java
@@ -0,0 +1,13 @@
+package com.example.juankno4.mediamanzana;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main17ActivityJK extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main17_jk);
+    }
+}
