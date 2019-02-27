fce791125e3249ce9130751d3a354f2f34b4c726
juanCano29 <juankno4@gmail.com>
2019-02-27 00:27:55 -0600

    sobre

--- app/src/main/java/com/example/juankno4/mediamanzana/Main7Activity.java
+++ app/src/main/java/com/example/juankno4/mediamanzana/Main7Activity.java
@@ -10,54 +10,22 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class Main7Activity extends AppCompatActivity implements View.OnClickListener {
-ImageView imgd;
-TextView txt1d,txt2d;
-Button btncerrar;
-Integer id = 0;
+public class Main7Activity extends AppCompatActivity  {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main7);
-        btncerrar = findViewById(R.id.btncerrar);
 
-        btncerrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                onBackPressed();
-                finish();
-            }
-        });
-    }
 
-    @Override
-    public void onClick(View v) {
-       showDialog(v.getId());
     }
 
 
 
-    @Override
-    protected Dialog onCreateDialog(int id)
-    {
-        Dialog dialogo = new Dialog(this);
 
-        Window w = dialogo.getWindow();
 
-       int flag = WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER;
 
-       w.setFlags(flag,flag);
-
-       dialogo.setTitle("Dialogo Basico");
-
-       dialogo.setContentView(R.layout.activity_main7);
-
-       imgd = (ImageView) dialogo.findViewById(R.id.crz);
-       txt1d = (TextView) dialogo.findViewById(R.id.textoD);
-       txt2d = (TextView) dialogo.findViewById(R.id.texto0D);
-
-    return dialogo;
-    }
 }
